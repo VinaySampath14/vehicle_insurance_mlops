dcdbc20a0ed74a11163efb6770b594b21834c06d
Vinay Sampath Kumar Vudumula <vinaysampathkumar.vudumula@gmail.com>
2026-04-07 00:26:38 +0200

    add project skeleton and set up MongoDB

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vinay Sampath Kumar Vudumula",
+    author_email="vinaysampathkumar.vudumula@gmail.com",
+    packages=find_packages()
+)
